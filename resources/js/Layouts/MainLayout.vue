0b31ce91c705991b22ef365673b07814b8f0e019
PayamAnari <anari.p62@gmail.com>
2023-12-23 13:40:10 +0100

    Add search

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -17,10 +17,28 @@ let showCreatePost = ref(false);
 <template>
     <div id="MainLayout" class="w-full h-screen">
       <div v-if="$page.url === '/'" id="TopNavHome" class="fixed z-30 md:hidden block w-full bg-white h-[61px] border-b border-b-gray-300">
-      <div class="flex items-center justify-center h-full">
+      <div class="flex items-center justify-between h-full">
        <Link href="/">
-        <img class="w-[105px] ml-6 cursor-ponter" src="/insta-logo.png">
+        <img class="w-[105px] ml-6 cursor-pointer" src="/insta-logo.png">
        </Link>
+
+       <div class="flex items-center w-[50%]">
+        <div class="flex items-center w-full bg-gray-100 rounded-lg">
+            <Magnify class="pl-4" fillColor="#8E8e8e" :size="27" />
+            <input
+            type="text"
+            placeholder="Search"
+            class="
+            bg-transparent
+            w-full
+            placeholder-[#8E8E8E]
+            border-0
+            ring-0
+            focus:ring-0
+            "
+            >
+        </div>
+      </div>
       </div>
       </div>
     </div>
