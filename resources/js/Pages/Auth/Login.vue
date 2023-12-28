448ba967a8b087a1e8cf8468392b3322a2709969
PayamAnari <anari.p62@gmail.com>
2023-12-28 13:42:33 +0100

    Add forgot password

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -67,20 +67,24 @@ const submit = () => {
                     Log in
                 </PrimaryButton>
 
-            <div class="block mt-4">
-                <label class="flex items-center">
-                    <Checkbox name="remember" v-model:checked="form.remember" />
-                    <span class="ms-2 text-sm text-gray-600">Remember me</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
+            <div class="flex items-center justify-center mt-4">
                 <Link
                     v-if="canResetPassword"
                     :href="route('password.request')"
-                    class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    class="text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
                     Forgot your password?
+                </Link>
+
+            </div>
+            <div class="flex items-center justify-center mt-4">
+                   Don't have an account?
+                <Link
+                    v-if="canResetPassword"
+                    :href="route('register')"
+                    class="pl-1 font-bold text-blue-600 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                    Sign up
                 </Link>
 
             </div>
