8a45b63860b65970adba164d94203e6639b5866c
PayamAnari <anari.p62@gmail.com>
2023-12-31 12:58:24 +0100

    Modify fileservice.php

--- app/Services/FileService.php
+++ app/Services/FileService.php
@@ -6,4 +6,15 @@ use Image;
 class FileService
 { 
     public function updateFile($model, $request, $type)
+    {
+        if(!empty($model->file)) {
+          $currentFile = public_path() . $model->file;
+
+          if(file_exists($currentFile) && $currentFile != public_path() . '/user-placeholder.png') {
+            unlink($currentFile);
+          }
+        }
+
+        $file = null;
+    }
 }
