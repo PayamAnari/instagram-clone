6323bcfe16dbf04f222b71719a8c56e8c5fc5a00
PayamAnari <anari.p62@gmail.com>
2023-12-31 13:34:35 +0100

    Add store route and destroy route

--- routes/web.php
+++ routes/web.php
@@ -26,9 +26,12 @@ Route::get('/user', function () {
 
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+       Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+       Route::delete('posts/{id}', [PostController::class, 'destroy'])->name('posts.destroy');
+    // Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    // Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    // Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 require __DIR__.'/auth.php';
