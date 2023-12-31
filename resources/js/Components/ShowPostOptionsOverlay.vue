d7003d71e5f8cd9bdc795b48ccf83f4bc5a0d9a1
PayamAnari <anari.p62@gmail.com>
2023-12-31 11:03:12 +0100

    Modify showpost

--- resources/js/Components/ShowPostOptionsOverlay.vue
+++ resources/js/Components/ShowPostOptionsOverlay.vue
@@ -1,4 +1,10 @@
 <script setup>
+import { toRefs } from "vue";
+
+defineEmits(["close", "deleteSelected"]);
+const props = defineProps({ deleteType: String, id:Number});
+
+const { deleteType, id } = toRefs(props);
 
 </script>
 
