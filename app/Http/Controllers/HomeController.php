37f4061643e3613233cd4cc26ef66d210e18178d
PayamAnari <anari.p62@gmail.com>
2024-01-01 12:03:24 +0100

    Modify home controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
+use Inertia\Inertia;
 
 class HomeController extends Controller
 {
-    //
+    public function index()
+    {
+       $posts = Post::orderBy('created_at', 'desc')->get();
+       return Inertia::render('Home', [
+        'posts' => new AllPostsCollection($posts),
+        'allUsers' => User::all(),
+       ]);
+    }
 }
