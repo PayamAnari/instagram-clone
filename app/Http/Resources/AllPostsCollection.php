ea38b5b59a7e91eb6955edfaaaf8e1c5a8d578d1
PayamAnari <anari.p62@gmail.com>
2024-01-01 12:20:08 +0100

    Modify allpostscollection

--- app/Http/Resources/AllPostsCollection.php
+++ app/Http/Resources/AllPostsCollection.php
@@ -12,8 +12,34 @@ class AllPostsCollection extends ResourceCollection
      *
      * @return array<int|string, mixed>
      */
-    public function toArray(Request $request): array
+    public function toArray(Request $request)
     {
-        return parent::toArray($request);
+        return $this->collection->map(function ($post) {
+            return [
+                'id' => $post->id,
+                'text' => $post->text,
+                'file' => $post->file,
+                'created_at' => $post->created_at->format('M D Y'),
+                'comments' => $post->comments->map(function ($comment) {
+                    return [
+                        'id' => $comment->id,
+                        'text' => $comment->text,
+                        'user' => [
+                            'id' => $comment->user->id,
+                            'name' => $comment->user->name,
+                            'file' => $comment->user->file,
+                        ],
+                    ];
+                }),
+                'Likes' => $post->likes->map(function ($like) {
+                  return [
+                    'id' => $like->id,
+                    'user_id' => $like->user_id,
+                    'post_id' => $like->post_id,
+                  ];
+        
+                }),
+            ];
+        });
     }
 }
