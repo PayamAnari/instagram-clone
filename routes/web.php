b8bb69b374777cdc4276d9ccfd0bb1b0846b7221
PayamAnari <anari.p62@gmail.com>
2024-01-02 14:12:58 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,14 @@
 <?php
 
+use App\Http\Controller\LikeController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PostController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -29,7 +31,12 @@ Route::middleware('auth')->group(function () {
 
        Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
        Route::delete('posts/{id}', [PostController::class, 'destroy'])->name('posts.destroy');
-   
+       
+       Route::post('/comments/{id}', [CommentController::class, 'store'])->name('comments.store');
+       Route::delete('/comments/{id}', [CommentController::class, 'destroy'])->name('comments.destroy');
+
+       Route::post('/likes', [LikeController::class, 'store'])->name('likes.store');
+       Route::delete('likes/{id}', [LikeController::class, 'destroy'])->name('likes.destroy');
 });
 
 require __DIR__.'/auth.php';
