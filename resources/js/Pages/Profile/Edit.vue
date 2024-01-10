2541bd77e4d2b32da22e42b03a4df19d152aa358
PayamAnari <anari.p62@gmail.com>
2024-01-10 11:42:01 +0100

    Add Done and cancel button

--- resources/js/Pages/Profile/Edit.vue
+++ resources/js/Pages/Profile/Edit.vue
@@ -4,7 +4,8 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import DeleteUserForm from './Partials/DeleteUserForm.vue';
 import UpdatePasswordForm from './Partials/UpdatePasswordForm.vue';
 import UpdateProfileInformationForm from './Partials/UpdateProfileInformationForm.vue';
-import { Head } from '@inertiajs/vue3';
+import { Head, Link } from '@inertiajs/vue3';
+
 
 defineProps({
     mustVerifyEmail: {
@@ -23,8 +24,12 @@ defineProps({
 
     <AuthenticatedLayout>
         <template #header>
+          <div class="flex justify-between">
+            <Link :href="route('users.show', { id: $page.props.auth.user.id })" class="font-semibold text-blue-500 cursor-pointer mt-1">Cancel</Link>
             <h2 class="font-semibold text-2xl text-center text-gray-800 leading-tight">Edit Profile</h2>
-        </template>
+            <Link href="/" class="font-semibold text-blue-500 cursor-pointer mt-1">Done</Link>
+          </div>
+          </template>
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
                 <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
